149ce5c2aab5ff132d94930c91e9be93e7e313e6
Yskinator <yski2moreletters@gmail.com>
2016-07-19 15:13:23 +0300

    Converted to NetBeans project.

---
+++ src/squirrelbotnetbeans/SquirrelBotNetbeans.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package squirrelbotnetbeans;
+
+/**
+ *
+ * @author villtann
+ */
+public class SquirrelBotNetbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
